fcc7e5b10127ad5ad5c1e0fa0be01fc2becb0bb3
mahi1212 <mahi121.mr@gmail.com>
2021-09-19 16:31:56 +0600

    user and todo API called and showed

--- src/App.js
+++ src/App.js
@@ -9,8 +9,72 @@ function App() {
       <Workshop></Workshop>
       <Counter name='Even' number='0'></Counter>
       <Counter name='Odd' number='1'></Counter>
+      <Employee></Employee>
     </div>
   );
+}
+function Employee(){
+  // User Details API Call
+  const [user, setUser] = useState([])
+  useEffect( ()=>{
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res=> res.json())
+    .then(data=> setUser(data))
+  }, [])
+  // Todo API Call
+  const [todos, setTodos] = useState([])
+  useEffect(()=>{
+    fetch('https://jsonplaceholder.typicode.com/todos?userId=1')
+    .then(res=> res.json())
+    .then(data=> setTodos(data))
+  },[])
+
+  return(
+    <div>
+      <h2>EMPLOYEE DETAILS</h2>
+      <div className='employeeGrid'>
+        <div style ={{width:'65%'}} className='left-employee'>
+          {
+            user.map(user => <Details name={user.name} email={user.email} phone={user.phone} add={user.address.city}></Details>)
+          } 
+        </div>
+        <div style={{width:'35%'}}>
+          <h3 style={{backgroundColor: '#0d8880',color:'#fff', marginTop: '2rem', padding: '5px', borderRadius:'10px'} }>Todo</h3>
+          {
+            todos.map(todo => <Todos title={todo.title} id={todo.id}></Todos>)
+          }
+        </div>
+      </div>
+    </div>
+  )
+}
+
+function Todos(props){
+  console.log(props)
+  const myStyle = {
+    backgroundColor : '#000',
+    color:'#fff',
+    margin: '10px 0',
+    borderRadius: '10px',
+    padding:'10px 5px'
+  }
+  return(
+    <div style={myStyle}>
+      <h5>Work number: {props.id}</h5>
+      <h5>Work Name : {props.title}</h5>
+    </div>
+  )
+}
+
+function Details(props){
+  return(
+    <div className='details-style' >
+      <h4 style={{color:'white'}}>Name : {props.name.toUpperCase().slice(0,8)}</h4>
+      <p>Email : {props.email}</p>
+      <p>Mobile: {props.phone.slice(2,12)}</p>
+      <p>City: {props.add}</p>
+    </div>
+  )
 }
 
 function Counter(props){
